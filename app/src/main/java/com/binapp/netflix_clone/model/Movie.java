a3949f253113b3d4d2d200a3c2154c50684160fb
sabinabernardes <sabina.ricardok@gmail.com>
2021-01-14 18:53:54 -0300

    Classe model Movie

---
+++ app/src/main/java/com/binapp/netflix_clone/model/Movie.java
@@ -0,0 +1,14 @@
+package com.binapp.netflix_clone.model;
+
+public class Movie {
+    private int coverUrl;
+
+    public int getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(int coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+
+}
