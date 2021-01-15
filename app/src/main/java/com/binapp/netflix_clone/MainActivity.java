2f47b666553bb7187e5d2fe951ddb1e31b5309c5
sabinabernardes <sabina.ricardok@gmail.com>
2021-01-15 14:47:32 -0300

    Layout tela principal ok + rolagem para os lados

--- app/src/main/java/com/binapp/netflix_clone/MainActivity.java
+++ app/src/main/java/com/binapp/netflix_clone/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 
+import com.binapp.netflix_clone.model.Category;
 import com.binapp.netflix_clone.model.Movie;
 
 import java.util.ArrayList;
@@ -27,53 +28,110 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerview = findViewById(R.id.reclycler_view_main);
-        List<Movie> movies = new ArrayList<>();
-        for (int i = 0; i <30 ; i++) {
-            Movie movie = new Movie();
-            movie.setCoverUrl(R.drawable.movie);
-            movies.add(movie);
+        List<Category> categories = new ArrayList<>();
 
+        for (int j = 0; j <10 ; j++) {
+            Category category = new Category();
+            category.setName("cat" + j);
+
+
+                List<Movie> movies = new ArrayList<>();
+                for (int i = 0; i < 30; i++) {
+                    Movie movie = new Movie();
+                    movie.setCoverUrl(R.drawable.movie);
+                    movies.add(movie);
+
+            }
+            category.setMovies(movies);
+            categories.add(category);
         }
-    mainAdapter= new MainAdapter(movies);
+    mainAdapter= new MainAdapter(categories);
         recyclerview.setLayoutManager(new LinearLayoutManager(this,
-                LinearLayoutManager.HORIZONTAL,
+                LinearLayoutManager.VERTICAL,
                 false));
         recyclerview.setAdapter(mainAdapter);
 
     }
-    private static class MovieHolder extends RecyclerView.ViewHolder{
 
-         final ImageView movie_image;
 
-        public MovieHolder(@NonNull View itemView) {
+
+    public static class CategoryHolder extends RecyclerView.ViewHolder {
+
+
+        TextView textViewTitle;
+        RecyclerView recyclerViewMovie;
+
+        public CategoryHolder(@NonNull View itemView) {
             super(itemView);
-            movie_image = itemView.findViewById(R.id.movie_image);
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            recyclerViewMovie = itemView.findViewById(R.id.recyclerViewMovie);
         }
     }
-    private class MainAdapter extends RecyclerView.Adapter<MovieHolder>{
-        private final List<Movie> movies;
 
-        private MainAdapter(List<Movie> movies) {
-            this.movies = movies;
+    private class MainAdapter extends RecyclerView.Adapter<CategoryHolder> {
+        private final List<Category> categories;
+
+        private MainAdapter(List<Category> categories) {
+            this.categories = categories;
         }
 
         @NonNull
         @Override
-        public MovieHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        public CategoryHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-            return new MovieHolder(getLayoutInflater().inflate(R.layout.movie_item,parent,false));
+            return new CategoryHolder(getLayoutInflater().inflate(R.layout.category_item, parent, false));
         }
 
         @Override
-        public void onBindViewHolder(@NonNull MovieHolder holder, int position) {
-            Movie movie = movies.get(position);
-            holder.movie_image.setImageResource(movie.getCoverUrl());
+        public void onBindViewHolder(@NonNull CategoryHolder holder, int position) {
+            Category category = categories.get(position);
+            holder.textViewTitle.setText(category.getName());
+            holder.recyclerViewMovie.setAdapter(new MovieAdapter(category.getMovies()));
+            holder.recyclerViewMovie.setLayoutManager(new LinearLayoutManager(getBaseContext(), RecyclerView.HORIZONTAL, false));
 
         }
 
         @Override
         public int getItemCount() {
-            return movies.size();
+            return categories.size();
         }
+
+    }
+    private static class MovieHolder extends RecyclerView.ViewHolder{
+
+        final ImageView imageViewCover;
+
+
+        public MovieHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewCover = itemView.findViewById(R.id.movie_image);
+        }
+    }
+
+        private class MovieAdapter extends RecyclerView.Adapter<MovieHolder>{
+            private final List<Movie> movies;
+
+            private MovieAdapter(List<Movie> movies) {
+                this.movies = movies;
+            }
+
+
+            @NonNull
+            @Override
+            public MovieHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                return new MovieHolder(getLayoutInflater().inflate(R.layout.movie_item,parent,false));
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull MovieHolder holder, int position) {
+                Movie movie = movies.get(position);
+                holder.imageViewCover.setImageResource(movie.getCoverURl());
+            }
+
+            @Override
+            public int getItemCount() {
+                return movies.size();
+            }
     }
 }
--- app/src/main/java/com/binapp/netflix_clone/model/Movie.java
+++ app/src/main/java/com/binapp/netflix_clone/model/Movie.java
@@ -3,12 +3,12 @@ package com.binapp.netflix_clone.model;
 public class Movie {
     private int coverUrl;
 
-    public int getCoverUrl() {
-        return coverUrl;
-    }
+
 
     public void setCoverUrl(int coverUrl) {
         this.coverUrl = coverUrl;
     }
 
+    public int getCoverURl() {return coverUrl;
+    }
 }
